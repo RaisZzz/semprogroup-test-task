db60b5eb9fc20248acd44fecba8cb34b84ccb11f
RaisZzz <beats.tapping@gmail.com>
2025-05-27 20:24:02 +0300

    ADD: Popup, open, close

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -12,18 +12,58 @@ const initAutoScrollTexts = () => {
 
     container.append(duplicateText);
 
-    trigger.addEventListener('mouseenter', (e) =>
+    trigger.addEventListener('mouseenter', () =>
       container.classList.add('auto-scroll-text--active')
     );
 
-    trigger.addEventListener('mouseleave', (e) =>
+    trigger.addEventListener('mouseleave', () =>
       container.classList.remove('auto-scroll-text--active')
     );
   }
 };
 
+const openPopup = (popup) => {
+  if (!popup) return;
+  popup.classList.add('popup--active');
+  requestAnimationFrame(() => popup.classList.add('popup--showed'));
+};
+
+const closePopup = (popup) => {
+  if (!popup) return;
+  popup.classList.remove('popup--showed');
+
+  const transitionDuration =
+    parseFloat(getComputedStyle(popup).transitionDuration) * 1000 || 300;
+  setTimeout(() => popup.classList.remove('popup--active'), transitionDuration);
+};
+
+const initPopups = () => {
+  // Init popup close buttons
+  const closeButtons = document.querySelectorAll('[data-close-popup]');
+  for (const close of closeButtons) {
+    close.addEventListener('click', () => {
+      const popupId = close.dataset.closePopup;
+      const popup = document.getElementById(popupId);
+      if (!popup) return;
+
+      closePopup(popup);
+    });
+  }
+
+  // Init popup triggers
+  const popupTriggers = document.querySelectorAll('[data-popup-id]');
+  for (const trigger of popupTriggers) {
+    const popupId = trigger.dataset.popupId;
+    const popup = document.getElementById(popupId);
+    if (!popup) return;
+
+    trigger.addEventListener('click', () => openPopup(popup));
+  }
+};
+
 const onDOMLoaded = () => {
   initAutoScrollTexts();
+  initPopups();
 };
 
 document.addEventListener('DOMContentLoaded', onDOMLoaded);
